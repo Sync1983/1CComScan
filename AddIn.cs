b6b4b41b12dd48a9846b6c22ee8a16ca4a90ab8e
Sync <Sync@SYNCPC>
2024-06-28 03:53:07 +0300

    Добавьте файлы проекта.

---
+++ AddIn.cs
@@ -0,0 +1,131 @@
+﻿using OneC.ExternalComponents;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using NAPS2.Wia;
+using System.Linq;
+
+
+namespace OneC.ExternalComponents {
+	[Guid("7166906E-EAC8-43D1-9B32-4FFE8FDAEEC3")]
+	[ProgId("AddIn.Com1CScan")]
+	public class Com1CScan: ExtComponentBase {
+
+		private const string Path = "C:\\Users\\Public\\hs.log";		
+		private WiaDeviceManager _deviceManager;
+		private uint _lastError = 0;
+
+		public Com1CScan() {
+			
+			ComponentName = "_1CComScan";
+
+			_deviceManager = new WiaDeviceManager();
+
+		}
+
+		~Com1CScan() {
+			Disconnect();
+		}		
+
+		protected void WriteLog(string msg) {
+			File.AppendAllText(Path, msg + Environment.NewLine);
+		}
+
+		[Export1c]
+		public string GetLastError() {
+			
+			string result = "";
+
+			switch(_lastError) {
+
+				case 0x80210003:
+					result = "There are no documents in the document feeder.";
+					break;
+
+				case 0x80210015:
+					result = "No scanner device was found. Make sure the device is online, connected to the PC, and has the correct driver installed on the PC.";
+					break;
+
+				case 0x80210005:
+					result = "The device is offline. Make sure the device is powered on and connected to the PC.";
+					break;
+
+				case 0x80210002:
+					result = "Paper is jammed in the scanner's document feeder.";
+					break;
+
+				case 0x80210006:
+					result = "The device is busy. Close any apps that are using this device or wait for it to finish and then try again.";				
+					break;
+
+				case 0x80210016:
+					result = "One or more of the device’s cover is open.";
+					break;
+
+				case 0x8021000A:
+					result = "ommunication with the WIA device failed. Make sure that the device is powered on and connected to the PC. If the problem persists, disconnect and reconnect the device.";
+					break;
+
+				case 0x8021000D:
+					result = "The device is locked. Close any apps that are using this device or wait for it to finish and then try again.";
+					break;
+
+				case 0x8021000C:
+					result = "There is an incorrect setting on the WIA device.";
+					break;
+
+				case 0x80210017:
+					result = "The scanner's lamp is off.";
+					break;
+
+				case 0x80210007:
+					result = "The device is warming up.";
+					break;
+
+				default:
+					result = "Всё хорошо";
+					break;
+			}
+			
+			return result;
+		}
+
+		[Export1c]
+		public int GetScanCount() {
+			
+			var infos = _deviceManager.GetDeviceInfos();			
+			return infos.Count();
+
+		}
+
+		[Export1c]
+		public string GetScanList() {
+
+			try{
+				
+				WiaDevice device = _deviceManager.PromptForDevice();
+				return device.Name() + "^;^" + device.Id();
+
+			}catch(WiaException ex){
+				_lastError = ex.ErrorCode;
+			}			
+
+			return "";
+
+		}
+
+		[Export1c]
+		public bool StartScan(string id) {
+
+			var _dev = _deviceManager.FindDevice(id);
+			//_dev.FindSubItem
+			return true;
+		}
+
+		[Export1c]
+		public void Disconnect() {
+
+		}
+
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.EnterpriseServices;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Com1CScan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Com1CScan")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ApplicationActivation(ActivationOption.Library)]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(true)]
+[assembly: AssemblyKeyFile("keys.snk")]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("D49121A7-5AD0-41E6-A3D2-C14DC1801A73")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
